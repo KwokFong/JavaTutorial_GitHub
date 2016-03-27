76189913c3851d3eac88e730342b9c24bb595a4e
KwokFong <kwokfonggit@gmail.com>
2016-03-26 20:18:54 -0700

    Adding setUp() and tearDown()
    Adding setUp() and tearDown() to test.

--- UdemyJavaTutorial/src/test/java/javatutorial21/Tutorial21_ToStringTest.java
+++ UdemyJavaTutorial/src/test/java/javatutorial21/Tutorial21_ToStringTest.java
@@ -1,5 +1,6 @@
 package javatutorial21;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import javatutorial21.Frog;
@@ -7,61 +8,46 @@ import static org.assertj.core.api.Assertions.*;
 
 public class Tutorial21_ToStringTest {
 	
-	public Frog frog1;
-	public Frog frog2;
-	
+	private Frog frog1;
+	private Frog frog2;
+		
 	@Before
-	public void setup(){
-
+	public void setUp(){
+		frog1 = new Frog(7, "Freddy");
+		frog2 = new Frog(5, "Roger");
 	}
 
 	@Test
 	public void checkObjectsNotNull(){
-		Frog frog1 = new Frog(7, "Freddy");
-		Frog frog2 = new Frog(5, "Roger");
-		
 		assertThat(frog1).isNotNull();
 		assertThat(frog2).isNotNull();
 	}
 	
 	@Test
 	public void checkObjects_toString(){
-		Frog frog1 = new Frog(7, "Freddy");
-		Frog frog2 = new Frog(5, "Roger");
-		
 		assertThat(frog1.toString()).as("Check frog1's toString() value").isEqualTo("7   : Freddy");
 		assertThat(frog2.toString()).as("Check frog2's toString() value").isEqualTo("5   : Roger");
-		
-		assertThat(frog1.toString()).startsWith("7");
-		assertThat(frog2.toString()).startsWith("5");
-		
-		assertThat(frog1.toString()).endsWith("Freddy");
-		assertThat(frog2.toString()).endsWith("Roger");
-		
-		assertThat(frog1).isNotEqualTo(frog2);
 	}	
 	
 	@Test
 	public void checkObjects_toString_startsWith_endsWith(){
-		Frog frog1 = new Frog(7, "Freddy");
-		Frog frog2 = new Frog(5, "Roger");
-		
 		assertThat(frog1.toString()).startsWith("7");
 		assertThat(frog2.toString()).startsWith("5");
 		
 		assertThat(frog1.toString()).endsWith("Freddy");
 		assertThat(frog2.toString()).endsWith("Roger");
-		
-		assertThat(frog1).isNotEqualTo(frog2);
 	}		
 	
 	@Test
 	public void checkObjects_isNotEqualTo(){	
-		Frog frog1 = new Frog(7, "Freddy");
-		Frog frog2 = new Frog(5, "Roger");
-		
 		assertThat(frog1).isNotEqualTo(frog2);
 	}			
+	
+	@After
+	public void tearDown(){
+		frog1 = null;
+		frog2 = null;
+	}
 }
 
 
