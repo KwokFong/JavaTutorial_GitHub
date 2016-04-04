9fa7c6b8e76d54f9218d0552e40b59928eba95e2
KwokFong <kwokfonggit@gmail.com>
2016-04-03 17:30:20 -0700

    Beautified App47.java
    Beautified App47.java

--- UdemyJavaTutorial/src/main/java/javatutorial47_passingbyvalue/App47.java
+++ UdemyJavaTutorial/src/main/java/javatutorial47_passingbyvalue/App47.java
@@ -12,39 +12,39 @@ public class App47 {
 	public static void main(String[] args) {
 		App47 app = new App47();
 		int value = 7;
-		
+
 		// ======================
-//		app.show(7);
-		
+		// app.show(7);
+
 		System.out.println("1. Value in main() is: " + value);
 		app.show(value);
 		System.out.println("4. Value in main() is: " + value);
 		System.out.println();
-		
+
 		// ======================
-		
+
 		Person person = new Person("Bob");
 		System.out.println("1. Person in main() is: " + person);
 		app.show(person);
 		System.out.println("4. Person in main() is: " + person);
 		System.out.println();
 	}
-	
-	public void show(int value){
+
+	public void show(int value) {
 		System.out.println("2. Value in show() is: " + value);
 		value = 8;
 		System.out.println("3. Value in show() is: " + value);
 	}
-	
-	public void show(Person person){
+
+	public void show(Person person) {
 		System.out.println("2. Person in show() is: " + person);
-		
+
 		person.setName("Sue");
-		
+
 		person = new Person("Mike");
-//		person.setName("Sue");
-		
+		// person.setName("Sue");
+
 		System.out.println("3. Person in show() is: " + person);
 	}
-	
+
 }
